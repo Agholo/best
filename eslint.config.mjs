de0276063c989e86ef50239864519fdd459966a6
Asatryan Aghasi <aghasi2004@mail.com>
2025-12-11 21:46:09 +0400

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,24 +3,24 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  {
-    rules: {
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "no-mixed-spaces-and-tabs": "error",
-      "no-trailing-spaces": "error",
-      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
-    },
-  },
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
+	{
+		rules: {
+			indent: ["error", 2, { SwitchCase: 1 }],
+			"no-mixed-spaces-and-tabs": "error",
+			"no-trailing-spaces": "error",
+			"no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+		},
+	},
 ]);
 
 export default eslintConfig;
